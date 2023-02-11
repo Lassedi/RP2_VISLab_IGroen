9dac545744461aab8dbe0c0bd323440d8729952f
lassedi <lassedi@outlook.de>
2023-02-11 15:12:31 +0100

    Fixed prepare_RF_data

--- RF_sing_elect_individual_trials/plot_prf_sing_elect_trialinterval.m
+++ RF_sing_elect_individual_trials/plot_prf_sing_elect_trialinterval.m
@@ -4,11 +4,11 @@
 dataDir = '~/Documents/ECoG_PRF_categories/data';
 
 % Pick a subject
-subject = 'p10';
+subject = 'p11';
 loadName = fullfile(dataDir, 'derivatives','ECoGPreprocessed', sprintf('sub-%s_prfcatdata.mat', subject));
 load(loadName);
 %% select data for the specific electrode and pRF trials only
-electrode = "GB25";
+electrode = "GB103";
 elect_pRF_act = epochs(:,:,electrodes.name == electrode);
 
 % Plot everything
@@ -24,81 +24,81 @@ tile.TileSpacing = "tight";
 %Vertical
 nexttile
 %prepare data
-[xlabel, x_interval, data_VerLR] = prepare_rf_data(elect_pRF_act, "VERTICAL-L-R", events, t);
+[xlabel, x_interval, data_RF] = prepare_rf_data(elect_pRF_act, "VERTICAL-L-R", events, t);
 
-plot(data_VerLR, "LineWidth",1)
+plot(data_RF, "LineWidth",1)
 
 xticks(x_interval);
 xticklabels(xlabel);
 ax = gca();
-ax.XLim = [0, length(data_VerLR)];
+ax.XLim = [0, length(data_RF)];
 
 
 
 % Horizontal 
 nexttile
-[xlabel, x_interval, data_VerLR] = prepare_rf_data(elect_pRF_act, 'HORIZONTAL-U-D', events, t);
+[xlabel, x_interval, data_RF] = prepare_rf_data(elect_pRF_act, 'HORIZONTAL-U-D', events, t);
 
-plot(data_VerLR, "LineWidth",1)
+plot(data_RF, "LineWidth",1)
 
 xticks(x_interval);
 xticklabels(xlabel);
 ax = gca();
-ax.XLim = [0, length(data_VerLR)];
+ax.XLim = [0, length(data_RF)];
 
 
 
 % Diagonal LU-RD
 nexttile
-[xlabel, x_interval, data_VerLR] = prepare_rf_data(elect_pRF_act, ...
+[xlabel, x_interval, data_RF] = prepare_rf_data(elect_pRF_act, ...
     "DIAGONAL-LU-RD", events, t);
 
-plot(data_VerLR, "LineWidth",1)
+plot(data_RF, "LineWidth",1)
 
 xticks(x_interval);
 ax = gca();
-ax.XLim = [0, length(data_VerLR)];
+ax.XLim = [0, length(data_RF)];
 ax.XTickLabel = xlabel;
 
 
 %Diagnoal RU-LD
 nexttile
-[xlabel, x_interval, data_VerLR] = prepare_rf_data(elect_pRF_act, ...
+[xlabel, x_interval, data_RF] = prepare_rf_data(elect_pRF_act, ...
     "DIAGONAL-RU-LD" , events, t);
 
-plot(data_VerLR, "LineWidth",1)
+plot(data_RF, "LineWidth",1)
 
 xticks(x_interval);
 ax = gca();
-ax.XLim = [0, length(data_VerLR)];
+ax.XLim = [0, length(data_RF)];
 ax.XTickLabel = xlabel;
 
 
 
 %Diagonal LD-RU
 nexttile
-[xlabel, x_interval, data_VerLR] = prepare_rf_data(elect_pRF_act, ...
+[xlabel, x_interval, data_RF] = prepare_rf_data(elect_pRF_act, ...
     "DIAGONAL-LD-RU" , events, t);
 
-plot(data_VerLR, "LineWidth",1)
+plot(data_RF, "LineWidth",1)
 
 xticks(x_interval);
 ax = gca();
-ax.XLim = [0, length(data_VerLR)];
+ax.XLim = [0, length(data_RF)];
 ax.XTickLabel = xlabel;
 
 
 
 %Diagnoal RD-LU
 nexttile
-[xlabel, x_interval, data_VerLR] = prepare_rf_data(elect_pRF_act, ...
+[xlabel, x_interval, data_RF] = prepare_rf_data(elect_pRF_act, ...
     "DIAGONAL-RD-LU" , events, t);
 
-plot(data_VerLR, "LineWidth",1)
+plot(data_RF, "LineWidth",1)
 
 xticks(x_interval);
 ax = gca();
-ax.XLim = [0, length(data_VerLR)];
+ax.XLim = [0, length(data_RF)];
 ax.XTickLabel = xlabel;
 
 
