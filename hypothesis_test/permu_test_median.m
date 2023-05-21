8e674937b0e301cd0cff5c082117f46db3a20254
lassedi <lassedi@outlook.de>
2023-05-21 18:06:16 +0200

    Hypothesis testing done

--- hypothesis_test/permu_test_median.m
+++ hypothesis_test/permu_test_median.m
@@ -1,11 +1,16 @@
-function [p] = permu_test_mean(distF, distH, n_samples)
+function [p] = permu_test_median(distF, distH, n_samples, median_mode)
 % calculates the p-value based on the permutation distribution gotten from
 % the permutation function and the original difference
 
+if ~exist("median_mode", "var")
+    median_mode = false;
+end
+
 %draw permutation samples of the mean difference
-[mean_dif, permu_dif] = permutation(distH, distF, n_samples);
+[median_dif, permu_dif] = permutation(distH, distF, n_samples, median_mode);
 
 %calculate the p - value with every difference larger than the observed
 %difference to get the percentile of the observed difference
-p = (sum(permu_dif > mean_dif) +1) / (n_samples + 1);
+p = (sum(permu_dif > median_dif) +1) / (n_samples + 1);
+
 end
--- hypothesis_test/permutation.m
+++ hypothesis_test/permutation.m
@@ -1,4 +1,12 @@
-function [mean_dif, permu_dif] = permutation(dist1, dist2, permutations)
+function [ori_dif, permu_dif] = permutation(dist1, dist2, permutations, mean_mode)
+% calculates the original difference of the chosen test statistic
+% (mean/median) and makes permutation samples based on the permutations
+% argument passed using the respective test statistic
+
+if ~exist("mean_mode", "var")
+    mean_mode = true;
+end
+
 
 %enforcing row vectors
 if iscolumn(dist1), dist1 = dist1'; end
@@ -6,7 +14,11 @@ if iscolumn(dist2), dist2 = dist2'; end
 
 %combine the two distributions into one & calc mean difference
 joinDist = [dist1, dist2];
-mean_dif = mean(dist1) - mean(dist2);
+if mean_mode
+    ori_dif = mean(dist1) - mean(dist2);
+else 
+    ori_dif = median(dist1) - median(dist2);
+end
 
 %randomly with replacement create new distributions & calcualte their
 %mean difference stored in permu_dif
@@ -14,6 +26,12 @@ permu_dif = zeros([1,permutations]);
 for p = 1:permutations
     p_dist1 = randsample(joinDist, length(dist1),false);
     p_dist2 = joinDist(~ismember(joinDist, p_dist1)); % get all datapoints that are not part of p_dist1
-    permu_dif(p) = mean(p_dist1) - mean(p_dist2);
+    
+    % calculate the difference with the relevant test statistic
+    if mean_mode
+        permu_dif(p) = mean(p_dist1) - mean(p_dist2);
+    else
+        permu_dif(p) = median(p_dist1) - median(p_dist2);
+    end
 end
 end
